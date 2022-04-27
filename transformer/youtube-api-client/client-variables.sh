7daee2d0f1557ad6d91609894ca0a7d750fa2622
BdN3504 <ein_armer_schlucker@gmx.de>
2022-04-28 01:02:19 +0200

    Fix path in client-variables.sh

--- transformer/youtube-api-client/client-variables.sh
+++ transformer/youtube-api-client/client-variables.sh
@@ -87,6 +87,7 @@ while getopts ":s:t:" opt; do
       sed -i -E "s%^(broadcastDescription=).*$%\1\"$broadcastDescription\"%g" "$self"
       ;;
     t)
+      self=$(realpath "$0")
       sed -i -E "s%^(broadcastTitle=).*$%\1\"$OPTARG\"%g" "$self"
       ;;
     \?)
