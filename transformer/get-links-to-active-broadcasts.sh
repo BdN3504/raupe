6d1332ea95d1e3a0eaa19b3d692431a9af09bce4
BdN3504 <ein_armer_schlucker@gmx.de>
2022-04-27 23:14:26 +0200

    Output a list of youtu.be links to all active livebroadcasts for your account

---
+++ transformer/get-links-to-active-broadcasts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+scriptPath=$(dirname $(realpath -s "$0"))
+youtubeApiClientPath="$scriptPath/youtube-api-client"
+. "$youtubeApiClientPath/client-variables.sh"
+echo "Checking authentication"
+"$youtubeApiClientPath/authentication-check.sh"
+
+responseFile="$youtubeApiClientPath/$broadcastsListActiveResponse"
+echo "These are the youtube urls to the currently active livebroadcasts of your channel:"
+/bin/bash "$youtubeApiClientPath/livebroadcasts-list-active.sh" | jq -r --arg prefix "https://youtu.be/" '$prefix + .items[].id'
