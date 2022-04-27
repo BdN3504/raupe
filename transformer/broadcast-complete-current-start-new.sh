2a2f757693b77b24c0401b992e5906c4ab3542f9
BdN3504 <ein_armer_schlucker@gmx.de>
2022-04-28 01:00:17 +0200

    Wait a bit to transition to live.

--- transformer/broadcast-complete-current-start-new.sh
+++ transformer/broadcast-complete-current-start-new.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-scriptPath=$(dirname $(realpath -s "$0"))
-youtubeApiClientPath="$scriptPath/youtube-api-client"
-numberOfDaysPassed=$("$youtubeApiClientPath/get-number-of-days-since-start-of-stream.sh")
+numberOfDaysPassed=$("./youtube-api-client/get-number-of-days-since-start-of-stream.sh")
 
-. "$youtubeApiClientPath/client-variables.sh"
 echo "Checking authentication"
-"$youtubeApiClientPath/authentication-check.sh"
-"$youtubeApiClientPath/livebroadcasts-transition-complete.sh"
-title="Caterpillar live broadcast - Vanessa Cardui - day $numberOfDaysPassed"
-"$youtubeApiClientPath/client-variables.sh" -t $title
-"$youtubeApiClientPath/livebroadcasts-insert.sh"
-"$youtubeApiClientPath/livebroadcasts-bind.sh"
-"$youtubeApiClientPath/livebroadcasts-transition-testing.sh"
-"$youtubeApiClientPath/livebroadcasts-transition-live.sh"
+./youtube-api-client/authentication-check.sh
+./youtube-api-client/livebroadcasts-transition-complete.sh
+newTitle="Caterpillar live broadcast - Vanessa Cardui - day $numberOfDaysPassed"
+./youtube-api-client/client-variables.sh -t "$newTitle"
+./youtube-api-client/livebroadcasts-insert.sh
+./youtube-api-client/livebroadcasts-bind.sh
+./youtube-api-client/livebroadcasts-transition-testing.sh
+sleep 10
+./youtube-api-client/livebroadcasts-transition-live.sh
