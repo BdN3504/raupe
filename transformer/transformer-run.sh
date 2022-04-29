fe7a31d52af1cb74adae9f1733f180cb0bc4bab1
BdN3504 <ein_armer_schlucker@gmx.de>
2022-04-29 09:30:11 +0200

    fix indentation

--- transformer/transformer-run.sh
+++ transformer/transformer-run.sh
@@ -12,55 +12,55 @@ streamKey=$(jq -r .cdn.ingestionInfo.streamName "$youtubeApiClientPath/$streamsI
 youtubeStreamAddress=$(jq -r .cdn.ingestionInfo.ingestionAddress  "$youtubeApiClientPath/$streamsInsertResponse")
 
 echo "Going to use this ffmpeg command: \
-	  ffmpeg -re \
-	  -rtsp_transport tcp \
-	  -i \"rtsp://$rtspServerHost:$rtspServerPort$rtspStreamPath\" \
-	  -f lavfi \
-	  -i anullsrc=channel_layout=stereo:sample_rate=44100 \
-	  -c:a libmp3lame -ab 32k -ar 44100 \
-	  -framerate $videoStreamFramerate \
-	  -b:v $videoStreamBitrate \
-	  -maxrate $videoStreamBitrate \
-	  -video_size $videoStreamResolution \
-	  -c:v copy \
-	  -threads 2 \
-	  -bufsize $videoStreamBufferSize \
-	  -f flv \"$youtubeStreamAddress/$streamKey\"" >> "$logFile"
+ffmpeg -re \
+-rtsp_transport tcp \
+-i \"rtsp://$rtspServerHost:$rtspServerPort$rtspStreamPath\" \
+-f lavfi \
+-i anullsrc=channel_layout=stereo:sample_rate=44100 \
+-c:a libmp3lame -ab 32k -ar 44100 \
+-framerate $videoStreamFramerate \
+-b:v $videoStreamBitrate \
+-maxrate $videoStreamBitrate \
+-video_size $videoStreamResolution \
+-c:v copy \
+-threads 2 \
+-bufsize $videoStreamBufferSize \
+-f flv \"$youtubeStreamAddress/$streamKey\"" >> "$logFile"
 ffmpegPid=$(ps -C ffmpeg -o pid=)
 if ((ffmpegPid > 0)); then
-	echo "ffmpeg pid gt 0" >> "$logFile"
-	firstCaptureTimestamp=$(timeout .5 strace -p$ffmpegPid -s9999 2>&1 | grep -oh "time=[[:digit:]]*:[[:digit:]]*:[[:digit:]]*\.[[:digit:]]*")
-	echo "$firstCaptureTimestamp" >> "$logFile"
-	sleep 1
-	secondCaptureTimestamp=$(timeout .5 strace -p$ffmpegPid -s9999 2>&1 | grep -oh "time=[[:digit:]]*:[[:digit:]]*:[[:digit:]]*\.[[:digit:]]*")
-	echo "$secondCaptureTimestamp" >> "$logFile"
-	if [ "$firstCaptureTimestamp" = "$secondCaptureTimestamp" ]; then
-	  if [ -z "$firstCaptureTimestamp" ]; then
-	    exit 0
+  echo "ffmpeg pid gt 0" >> "$logFile"
+  firstCaptureTimestamp=$(timeout .5 strace -p$ffmpegPid -s9999 2>&1 | grep -oh "time=[[:digit:]]*:[[:digit:]]*:[[:digit:]]*\.[[:digit:]]*")
+  echo "$firstCaptureTimestamp" >> "$logFile"
+  sleep 1
+  secondCaptureTimestamp=$(timeout .5 strace -p$ffmpegPid -s9999 2>&1 | grep -oh "time=[[:digit:]]*:[[:digit:]]*:[[:digit:]]*\.[[:digit:]]*")
+  echo "$secondCaptureTimestamp" >> "$logFile"
+  if [ "$firstCaptureTimestamp" = "$secondCaptureTimestamp" ]; then
+    if [ -z "$firstCaptureTimestamp" ]; then
+      exit 0
     fi
-	  echo "Same timestamps found, restarting ffmpeg" >> "$logFile"
-	  screen -S raupe -X quit
-	  sleep 1
+    echo "Same timestamps found, restarting ffmpeg" >> "$logFile"
+    screen -S raupe -X quit
+    sleep 1
     screen -S raupe -dm \
-	  ffmpeg -re \
-	  -rtsp_transport tcp \
-	  -i "rtsp://$rtspServerHost:$rtspServerPort$rtspStreamPath" \
-	  -f lavfi \
-	  -i anullsrc=channel_layout=stereo:sample_rate=44100 \
-	  -c:a libmp3lame -ab 32k -ar 44100 \
-	  -framerate $videoStreamFramerate \
-	  -b:v $videoStreamBitrate \
-	  -maxrate $videoStreamBitrate \
-	  -video_size $videoStreamResolution \
-	  -c:v copy \
-	  -threads 2 \
-	  -bufsize $videoStreamBufferSize \
-	  -f flv "$youtubeStreamAddress/$streamKey"
-	  echo "Started a new ffmpeg process" >> "$logFile"
-	else
+    ffmpeg -re \
+    -rtsp_transport tcp \
+    -i "rtsp://$rtspServerHost:$rtspServerPort$rtspStreamPath" \
+    -f lavfi \
+    -i anullsrc=channel_layout=stereo:sample_rate=44100 \
+    -c:a libmp3lame -ab 32k -ar 44100 \
+    -framerate $videoStreamFramerate \
+    -b:v $videoStreamBitrate \
+    -maxrate $videoStreamBitrate \
+    -video_size $videoStreamResolution \
+    -c:v copy \
+    -threads 2 \
+    -bufsize $videoStreamBufferSize \
+    -f flv "$youtubeStreamAddress/$streamKey"
+    echo "Started a new ffmpeg process" >> "$logFile"
+  else
     echo "Timestamps differ, no need to do anything, going to exit." >> "$logFile"
     exit 0
-	fi
+  fi
 else
   echo "ffmpeg is not running, starting" >> "$logFile"
   screen -S raupe -dm \
